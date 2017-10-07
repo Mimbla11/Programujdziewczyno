3debbfedc2173e0158b9fca482eadaaf2fbb9251
Monika Mazurek <mimbla11@gazeta.pl>
2017-10-07 16:15:51 +0200

    drugie zmiany

---
+++ js/app.js
@@ -0,0 +1,21 @@
+console.log("działa");
+'use strict';
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var forDropdown=document.querySelector('.for-dropdown');
+    var contents=document.querySelector('.dropdown');
+    forDropdown.addEventListener('mouseover', function() {
+        contents.style.display = "block";
+   
+    });
+   
+    forDropdown.addEventListener('mouseout', function() {
+        contents.style.display = "none";
+
+
+    });
+
+
+
+});
